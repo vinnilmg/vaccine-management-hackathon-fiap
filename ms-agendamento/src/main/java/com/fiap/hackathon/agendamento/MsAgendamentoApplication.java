72fe18f2d25f259c8f7a466b6edd1d62ee642a95
= <vini.delimagomes@gmail.com>
2025-02-02 20:29:32 -0300

    feat: init ms-agendamento

---
+++ ms-agendamento/src/main/java/com/fiap/hackathon/agendamento/MsAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.hackathon.agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgendamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAgendamentoApplication.class, args);
+    }
+
+}
