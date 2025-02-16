b1befddcb1e1f3d5caa956673748c3b0497867c9
= <vini.delimagomes@gmail.com>
2025-02-16 18:19:57 -0300

    fix: Corrigindo erros

--- ms-postos-vacina/src/main/resources/data.sql
+++ ms-postos-vacina/src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO funcionamento(id, dia_semana, horario_inicio, horario_fim)
 VALUES ('1', 'SEGUNDA', '07:00', '16:00'),
        ('2', 'TERCA', '08:00', '15:00');
 
-INSERT INTO posto (id, nome, endereco_id, tipo_local, funcionamento_id, status)
-VALUES ('1', 'UBS Jardim Vaz de Lima', '1', 'UBS', '1', 'ATIVO');
+INSERT INTO posto (id, nome, endereco_id, tipo_local, status)
+VALUES ('1', 'UBS Jardim Vaz de Lima', '1', 'UBS', 'ATIVO');
