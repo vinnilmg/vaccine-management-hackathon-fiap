9668ebaa48189ab70f755fe3b19c8829cf1f88d0
= <vini.delimagomes@gmail.com>
2025-02-19 12:25:11 -0300

    fix: Ajustes nos responses e mocks

--- ms-agendamento/src/main/java/com/fiap/hackathon/agendamento/infra/client/response/usuario/UsuarioResponse.java
+++ ms-agendamento/src/main/java/com/fiap/hackathon/agendamento/infra/client/response/usuario/UsuarioResponse.java
@@ -1,10 +1,13 @@
 package com.fiap.hackathon.agendamento.infra.client.response.usuario;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UsuarioResponse(
     Long id,
     String email,
     String nome,
     String cpf,
+    @JsonProperty("numeroCarteirinhaSUS")
     String numeroCarteira
 ) {
 }
