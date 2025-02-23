41ceb5f3d3a923745ea4f129de0cbd565a6adbdf
ramunizbh <48028670+ramunizbh@users.noreply.github.com>
2025-02-23 18:23:22 -0300

    Update data.sql
    atualização de dados

--- ms-postos-vacina/src/main/resources/data.sql
+++ ms-postos-vacina/src/main/resources/data.sql
@@ -7,3 +7,8 @@ VALUES ('SEGUNDA', '07:00', '16:00', 1),
 
 INSERT INTO endereco (rua, numero, complemento, bairro, cidade, estado, cep, posto_id)
 VALUES ('Rua Hortência Pedro', '200', null, 'Jardim Vaz de Lima', 'São Paulo', 'SP', '05833140', 1);
+
+INSERT INTO lote (numero, posto_vacinacao_id, vacina_id, validade, estoque)
+values  ('1111', 1, 2, '2025-03-05', 20),
+        ('2222', 1, 2, '2025-03-25', 10),
+        ('123456', 2, 2, '2025-03-15', 3);
